62e90647cdfe324ddd8787b7fa860d0182326799
ranjun <openote@126.com>
2025-11-18 23:19:01 +0800

    mem by redis

--- agent-by-imooc/agent/04-agent_chat-with-mem.py
+++ agent-by-imooc/agent/04-agent_chat-with-mem.py
@@ -1,5 +1,5 @@
 from langgraph.prebuilt import create_react_agent
-from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.redis import RedisSaver
 from pkg.chat_model.qwen import chat_model
 from langchain_community.agent_toolkits.file_management import FileManagementToolkit
 
@@ -9,15 +9,15 @@ from langchain_core.messages import HumanMessage
 DATA_DIR = "../data/conversations"
 
 def create_agent():
-    mem = MemorySaver()
     file_tools = FileManagementToolkit(root_dir=DATA_DIR).get_tools()
-    agent = create_react_agent(
-        model=chat_model,
-        tools=file_tools,
-        checkpointer=mem,
-        debug=True,
-    )
-    return agent
+    with RedisSaver.from_conn_string("redis://127.0.0.1:6379") as mem:
+        agent = create_react_agent(
+            model=chat_model,
+            tools=file_tools,
+            checkpointer=mem,
+            debug=True,
+        )
+        return agent
 
 def run_agent():
     config = RunnableConfig(configurable={"thread_id": 1})
